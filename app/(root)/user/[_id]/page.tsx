181f33841ff13dcb43a30e67b23e954bc3a70e74
noviciusss <samarthsin2006@gmail.com>
2025-02-18 16:21:49 +0530

    user profile

--- app/(root)/user/[_id]/page.tsx
+++ app/(root)/user/[_id]/page.tsx
@@ -1,8 +1,11 @@
 import { auth } from '@/auth';
 import { AUTHOR_BY_ID_QUERY } from '@/lib/queries';
 import { client } from '@/sanity/lib/client';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import React from 'react'
+
+export const experimental_ppr=true;
 
 const Page=async ({params}:{params:Promise<{id:string,}>})=>{
     const id = (await params).id;
@@ -12,6 +15,36 @@ const Page=async ({params}:{params:Promise<{id:string,}>})=>{
     const user = await client.fetch(AUTHOR_BY_ID_QUERY,{id});
     if(!user) return notFound();
     return <>{/**called react fragemnt */}
+      <section className='profile_card'>
+        <div className='profile-card'>
+            <div className='profile_title'>
+                <h3 className='text-24-black uppercase text-center line-clamp-1'>
+                    {user.name}
+                </h3>
+            </div>
+            <Image 
+                src = {user.image}
+                alt={user.name}
+                width={220}
+                height={220}
+                className='profile_image'
+                />
+            <p className='text-30-extrabold mt-7 text-center'>
+               @{user?.username}  
+            </p>
+            <p className='mt-1 text-center text-14-normal'>
+                {user?.bio}
+            </p>    
+        </div>
+        <div className='flex-1 flex flex-col gap-5 lg:-mt-5'>
+            <p className='text-30-bold'>
+                {session?.id===id?'Your':'All'}Project
+            </p>
+            <ul className='card_grid-sm'>
+
+            </ul>
+        </div>
+      </section>
     </>
 
 };

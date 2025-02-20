8e3763a0f933c5932f00ab754ec574ad8c013e17
Samarth Pratap Singh <samarthsin2006@gmail.com>
2025-02-21 00:54:20 +0530

    queries error

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,19 +1,20 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY = defineQuery(`
-  *[_type == "startup" && defined(slug.current) && (!defined($search) || title match $search || category match $search || author->name match $search)]
-  | order(_createdAt desc) {
-    _id,
-    title,
-    slug,
-    _createdAt,
-    author->{_id, name, image, bio},
-    views,
-    description,
-    category,
-    image
-  }
-`);
+export const STARTUPS_QUERY =
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+}`);
+
 
 export const STARTUP_BY_ID_QUERY = defineQuery(`
   *[_type == "startup" && _id == $id][0]{
@@ -62,20 +63,21 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
   }
 `);
 
-export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`
-  *[_type == "startup" && author._ref == $id && (!defined($search) || title match $search || category match $search)]
-  | order(_createdAt desc) {
-    _id,
-    title,
-    slug,
-    _createdAt,
-    author->{_id, name, image, bio},
-    views,
-    description,
-    category,
-    image
-  }
-`);
+export const STARTUPS_BY_AUTHOR_QUERY =
+  defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+}`);
+
 
 export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`
   *[_type == "playlist" && slug.current == $slug][0]{

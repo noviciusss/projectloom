b90371d60d1c3d22e94eaa9d3439c5a2c5f642bc
Samarth Pratap Singh <samarthsin2006@gmail.com>
2025-02-21 00:47:24 +0530

    queriers error

--- components/UserStrartup.tsx
+++ components/UserStrartup.tsx
@@ -1,11 +1,11 @@
-import { STARTUPS_BY_AUTHOR } from "@/lib/queries";
+import { STARTUPS_BY_AUTHOR_QUERY } from "@/lib/queries";
 import { client } from "@/sanity/lib/client";
 import React from "react";
 import StartupCard,{StartupTypeCard} from "./StartupCard";
 
 
 const UserStartups=async ({id}:{id:string})=>{
-   const startups = await client.fetch(STARTUPS_BY_AUTHOR,{id})
+   const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY,{id})
    
   return <>
     {startups.length>0 ? startups.map((startup:StartupTypeCard)=>(
